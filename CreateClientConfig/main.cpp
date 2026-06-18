0413aca67ee89d1771811a3eab9b596f2f3f0781
StefanSchmidt9999 <Stefan.Schmidt@StefanSchmidtHamburg.com>
2026-06-18 14:54:21 +0200

    Projektdateien hinzufügen.

---
+++ CreateClientConfig/main.cpp
@@ -0,0 +1,56 @@
+﻿#include "pch.h"
+
+
+
+int main()
+{
+    winrt::init_apartment();
+    
+	std::wcout << L"Hallo" << std::endl;
+
+    std::wstring clientId = L"CPP-CLIENT-001";
+
+    DATA_BLOB inputBlob;
+    inputBlob.pbData = reinterpret_cast<BYTE*>(const_cast<wchar_t*>(clientId.c_str()));
+
+    inputBlob.cbData = static_cast<DWORD>((clientId.size() + 1) * sizeof(wchar_t));
+
+    DATA_BLOB outputBlob;
+
+    BOOL ok = CryptProtectData(
+        &inputBlob,
+        L"ImpulsAufKurs ClientId",
+        nullptr,
+        nullptr,
+        nullptr,
+        0,
+        &outputBlob
+    );
+
+    if (!ok)
+    {
+        std::wcout << L"CryptProtectData fehlgeschlagen.\n";
+        return 1;
+    }
+
+    std::wstring folder = std::wstring(_wgetenv(L"LOCALAPPDATA")) + L"\\ImpulsAufKurs";
+
+    std::filesystem::create_directories(folder);
+
+    std::wstring filePath = folder + L"\\client.dat";
+
+    std::ofstream file(filePath, std::ios::binary);
+
+    file.write(reinterpret_cast<char*>(outputBlob.pbData), outputBlob.cbData);
+
+    file.close();
+
+    LocalFree(outputBlob.pbData);
+
+    std::wcout << L"ClientId wurde verschluesselt gespeichert:\n";
+    std::wcout << filePath << std::endl;
+    
+	system("pause"); 
+
+    return 0;
+}
---
+++ CreateClientConfig/pch.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <windows.h>
+#include <wincrypt.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+
+#include <winrt/base.h>
+
+#pragma comment(lib, "Crypt32.lib")
